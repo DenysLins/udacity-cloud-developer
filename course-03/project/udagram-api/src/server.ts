98b14714f330b1bd013b5e0d18c9ed5806eabf67
Denys Lins <denys.lins@hp.com>
2020-07-05 14:40:22 -0300

    Set udagram-api port to environment variable

--- course-03/project/udagram-api/src/server.ts
+++ course-03/project/udagram-api/src/server.ts
@@ -1,12 +1,12 @@
 import cors from 'cors';
 import express from 'express';
-import {sequelize} from './sequelize';
+import { sequelize } from './sequelize';
 
-import {IndexRouter} from './controllers/v0/index.router';
+import { IndexRouter } from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import { config } from './config/config';
+import { V0_FEED_MODELS, V0_USER_MODELS } from './controllers/v0/model.index';
 
 
 (async () => {
@@ -15,7 +15,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = config.udagram_api_port || 8080;
 
   app.use(bodyParser.json());
 
@@ -32,14 +32,14 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
+  app.get('/', async (req, res) => {
+    res.send('/api/v0/');
+  });
 
 
   // Start the Server
-  app.listen( port, () => {
-    console.log( `server running ${config.url}` );
-    console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server is running ans listening on port ${config.udagram_api_port}... Environment variables: ${JSON.stringify(config)}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
