4005c1b981486b6ee4baccd2b0889af42ee9d433
Denys Lins <denyslins@gmail.com>
2020-07-09 23:54:59 -0300

    Develop (#21)
    * Add starter code for project 3
    
    * Fix udagram-api to run in local machine
    
    * Add dockerfile and docker-compose
    
    * Add environment variable to docker-compose
    
    * Add travis config file
    
    * Move travis config file to root directory
    
    * Fix travis config file
    
    * Set udagram-api port to environment variable
    
    * Add dockerfile for udagram-frontend
    
    * Update travis to build udagram-frontend
    
    * Add aws environemnt variables
    
    * Add nginx reverse-proxy
    
    * Update travis to build nginx reverse-proxy
    
    * Add postgres k8 files
    
    * Add frontend k8s files
    
    * Add backend k8s files
    
    * Add nginx ingress
    
    * Fix frontend environment
    
    * Change env
    
    * Fix build in production
    
    * Fix nginx reverse proxy
    
    * Fix travis config file
    
    * Fix ingress-nginx redirect
    
    * Refact udagram-api into two services
    
    * Remove prod build from frontend
    
    * Finish project 3
    
    * Project 4 initial commit
    
    * Implements getTodos
    
    * Implements createTodo
    
    * Update serverless.yml
    
    Add validation to crateTodo
    Enable cors
    Add todosTable resource
    Define IAM
    Crate environment variables
    
    * Add validation to updateTodo request
    
    * Add deleteTodo handler
    
    * Fix handlers
    
    * Implements generateUploadUrl
    
    * Implements authorization
    
    * Remove json template from code
    
    * Finish course-04 project v1
    
    * Add specific role per function
    
    * Enable auth function
    
    * Add police for auth
    
    * Fix issues found in review
    
    Change scan to query
    Add AAWS Xray
    
    * Fix issues found in review
    
    Add data layer
    Add business layer
    
    Co-authored-by: Denys Lins <denys.lins@hp.com>

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -1,65 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
-
-import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
-import { createLogger } from '../../utils/logger'
-import * as utils from '../utils'
-
-const logger = createLogger('todos')
-const docClient = new AWS.DynamoDB.DocumentClient()
-const todoTable = process.env.TODO_TABLE
+import { updateTodo } from '../../businessLayer/todosBusinessLayer';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
-  logger.info('updateTodo', {
-    event
-  })
+  const message = await updateTodo(event)
 
-  const todoId = event.pathParameters.todoId
-  const userId = utils.getUserId(event)
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-
-  const param = {
-    TableName: todoTable,
-    Key: {
-      "todoId": todoId,
-      "userId": userId
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
     },
-    UpdateExpression: "set #tn = :n, dueDate=:dd, done=:d",
-    ExpressionAttributeNames: { '#tn': 'name' },
-    ExpressionAttributeValues: {
-      ":n": updatedTodo.name,
-      ":dd": updatedTodo.dueDate,
-      ":d": updatedTodo.done
-    }
-  }
-
-  return new Promise((resolve, reject) => {
-    docClient.update(param, (err, data) => {
-      if (err) {
-        logger.info('err', { err })
-        reject({
-          statusCode: 400,
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          },
-          body: JSON.stringify({
-            message: err
-          })
-        })
-      }
-      logger.info('data', { data })
-      resolve({
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-          message: "todo updated"
-        })
-      })
+    body: JSON.stringify({
+      message
     })
-  })
+
+  }
 
 }
